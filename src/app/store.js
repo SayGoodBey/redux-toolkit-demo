c2ea8656048a0e59d6be58f89aaafb2c6968300d
seansshang <seansshang@tencent.com>
2024-07-29 20:43:16 +0800

    feat: 初始化仓库

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 
 export const store = configureStore({
+  devTools: false,
+  enhancers: [],
   reducer: {
     counter: counterReducer,
   },
